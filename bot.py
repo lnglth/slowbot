1057b9690fffe8ab59dae1fb54810ffadb18a63d
Long Le <lthlong95@gmail.com>
2024-06-11 23:20:47 +0200

    feat: test ray functions

--- bot.py
+++ bot.py
@@ -1,11 +1,21 @@
 import logging
 
+import requests
 from fastapi import FastAPI
+from ray import serve
 
 logger = logging.getLogger(__name__)
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@serve.deployment
+@serve.ingress(app)
+class FastAPIDeployment:
+    @app.get("/")
+    async def root(self):
+        return {"message": "Hello World"}
+
+
+serve.run(FastAPIDeployment.bind(), route_prefix="/")
+
+print(requests.get("http://localhost:8000").json())
